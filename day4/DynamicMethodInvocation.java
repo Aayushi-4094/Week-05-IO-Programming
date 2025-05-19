a452e2c0e07afc44a720ee04949807ef2796f23b
Aayushi <aayushiagarwal4094@gmail.com>
2025-05-19 10:13:21 +0530

    dynamic method

---
+++ day4/DynamicMethodInvocation.java
@@ -0,0 +1,20 @@
+import java.lang.reflect.Method;
+import java.util.Scanner;
+
+class MathOperations {
+    public int add(int a, int b) { return a + b; }
+    public int subtract(int a, int b) { return a - b; }
+    public int multiply(int a, int b) { return a * b; }
+}
+
+public class DynamicMethodInvocation {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter method (add, subtract, multiply): ");
+        String methodName = sc.nextLine();
+
+        Method method = MathOperations.class.getMethod(methodName, int.class, int.class);
+        Object result = method.invoke(new MathOperations(), 10, 5);
+        System.out.println("Result: " + result);
+    }
+}
