e8229c328aeddfa3cbdf0992479df0ae7622d799
Aayushi <aayushiagarwal4094@gmail.com>
2025-05-19 10:11:07 +0530

    modify static field

---
+++ day4/ModifyStaticField.java
@@ -0,0 +1,16 @@
+import java.lang.reflect.Field;
+
+class Configuration {
+    private static String API_KEY = "OLD_KEY";
+}
+
+public class ModifyStaticField {
+    public static void main(String[] args) throws Exception {
+        Field field = Configuration.class.getDeclaredField("API_KEY");
+        field.setAccessible(true);
+
+        System.out.println("Original API_KEY: " + field.get(null));
+        field.set(null, "NEW_SECRET_KEY");
+        System.out.println("Modified API_KEY: " + field.get(null));
+    }
+}
