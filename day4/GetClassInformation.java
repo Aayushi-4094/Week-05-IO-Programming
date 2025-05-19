a66276a5b2589d02b5983b53c02aa0c094065d61
Aayushi <aayushiagarwal4094@gmail.com>
2025-05-19 09:59:26 +0530

    Added get class info

---
+++ day4/GetClassInformation.java
@@ -0,0 +1,24 @@
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class GetClassInformation {
+    public static void main(String[] args) throws Exception {
+        Class<?> clazz = Class.forName("java.util.ArrayList"); // You can replace this with any class name
+
+        System.out.println("Methods:");
+        for (Method method : clazz.getDeclaredMethods()) {
+            System.out.println(method);
+        }
+
+        System.out.println("\nFields:");
+        for (Field field : clazz.getDeclaredFields()) {
+            System.out.println(field);
+        }
+
+        System.out.println("\nConstructors:");
+        for (Constructor<?> constructor : clazz.getDeclaredConstructors()) {
+            System.out.println(constructor);
+        }
+    }
+}
